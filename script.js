28fba151486313ce8e13089d79ddb14675eb1ccd
Ritik Singh <sritiksingh246@gmail.com>
2025-10-10 16:47:12 +0530

    final add

--- script.js
+++ script.js
@@ -1,78 +1,58 @@
-function goUpBtn(){
-  window.scrollTo(0,0);
+function goUpBtn() {
+    window.scrollTo(0, 0);
 }
 
+function getCardShift(containerClass) {
+    const card = document.querySelector(containerClass + " .card, " + containerClass + " .scroll > div");
+    const style = window.getComputedStyle(card);
+    const marginLeft = parseFloat(style.marginLeft);
+    const marginRight = parseFloat(style.marginRight);
+    const width = card.getBoundingClientRect().width;
+    return width + marginLeft + marginRight;
+}
 
-
-const observer= new IntersectionObserver( entries => {
-    entries.forEach(entry => {
-        if(countStart==false && entry.isIntersecting){
-            countStart=true;
-            console.log(entry);
-        runCount();
-
-        }
-        
-    })
-} ,{threshold:0.5})
-
-observer.observe(document.querySelector(".container4"))
-
-click6 = 0;
 function left6() {
-  scroll6 = document.querySelector(".container6 .scroll");
-  card6 = document.querySelector(".container6 .card").getBoundingClientRect().width;;
-  scroll6.scrollLeft -= card6 + 18;
-  click6--;
+    const scroll = document.querySelector(".container6 .scroll");
+    const shift = getCardShift(".container6");
+    scroll.scrollLeft -= shift;
 }
 function right6() {
-  scroll6 = document.querySelector(".container6 .scroll");
-  card6 = document.querySelector(".container6 .card").getBoundingClientRect().width;;
-  if (click6 == 0) {
-    scroll6.scrollLeft += card6 + 18 + 18;
-    click6++;
-  } else {
-    scroll6.scrollLeft += card6 + 18;
-    click6++;
-  }
+    const scroll = document.querySelector(".container6 .scroll");
+    const shift = getCardShift(".container6");
+    scroll.scrollLeft += shift;
 }
 
-scroll7 = document.querySelector(".container7 .scroll");
-card7 = document.querySelector(".container7 .card").getBoundingClientRect().width;;
-click7 = 0;
 function left7() {
-  scroll7 = document.querySelector(".container7 .scroll");
-  card7 = document.querySelector(".container7 .card").getBoundingClientRect().width;;
-  scroll7.scrollLeft -= card7 + 11;
-  click7--;
+    const scroll = document.querySelector(".container7 .scroll");
+    const shift = getCardShift(".container7");
+    scroll.scrollLeft -= shift;
 }
 function right7() {
-  scroll7 = document.querySelector(".container7 .scroll");
-  card7 = document.querySelector(".container7 .card").getBoundingClientRect().width;;
-  if (click7 == 0) {
-    scroll7.scrollLeft += card7 + 11 + 11;
-    click7++;
-  } else {
-    scroll7.scrollLeft += card7 + 11;
-    click7++;
-  }
+    const scroll = document.querySelector(".container7 .scroll");
+    const shift = getCardShift(".container7");
+    scroll.scrollLeft += shift;
 }
 
-click8 = 0;
 function left8() {
-  scroll8 = document.querySelector(".container8 .scroll");
-  card8 = document.querySelector(".container8 .scroll > div").getBoundingClientRect().width;;
-  scroll8.scrollLeft -= card8 + 12;
-  click8--;
+    const scroll = document.querySelector(".container8 .scroll");
+    const shift = getCardShift(".container8");
+    scroll.scrollLeft -= shift;
 }
 function right8() {
-  scroll8 = document.querySelector(".container8 .scroll");
-  card8 = document.querySelector(".container8 .scroll > div").getBoundingClientRect().width;;
-  if (click8 == 0) {
-    scroll8.scrollLeft += card8 + 12 + 12;
-    click8++;
-  } else {
-    scroll8.scrollLeft += card8 + 12;
-    click8++;
-  }
+    const scroll = document.querySelector(".container8 .scroll");
+    const shift = getCardShift(".container8");
+    scroll.scrollLeft += shift;
 }
+
+const phoneNumber = "919780028787";
+document.querySelectorAll(".callBtn").forEach(button => {
+    button.addEventListener("click", function () {
+        window.location.href = `tel:+${phoneNumber}`;
+    });
+});
+
+document.querySelectorAll(".messageBtn").forEach(button => {
+    button.addEventListener("click", function () {
+        window.open(`https://wa.me/${phoneNumber}`, "_blank");
+    });
+});
